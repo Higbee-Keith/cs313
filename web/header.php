aaee8546b35bb3ee2fa7d68bd26c02bd5fe754ba
Keith Higbee <Keith Higbee>
2017-01-19 19:24:46 -0700

    fixed header.php

--- web/header.php
+++ web/header.php
@@ -1,7 +1,7 @@
 	<div class="w3-top">
 		<ul class="w3-navbar w3-black w3-xlarge w3-card-4">
 			<li class="w3-hover-white w3-left w3-large">
-				<a class="w3-hover-white w3-large" href="index.php" target="_blank">Home</a>
+				<a class="w3-hover-white w3-large" href="index.php">Home</a>
 			</li>
 			<li class="w3-hover-white w3-left w3-large">
 				<a href="assignments.php">Assignments</a>
