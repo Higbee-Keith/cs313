3efdbcef06f8b24470a623b8e89c69f718d30993
Keith Higbee <Keith Higbee>
2017-02-03 10:25:29 -0700

    possible fix to connect to db

--- web/week05/self/connect.php
+++ web/week05/self/connect.php
@@ -1,5 +1,29 @@
 <?php
-   try
+   // default Heroku Postgres configuration URL
+   $dbUrl = getenv('DATABASE_URL');
+
+   if (empty($dbUrl)) {
+      // example localhost configuration URL with postgres username and a database called cs313db
+      $dbUrl = "postgres://postgres:1@localhost:5432/postgres";
+   }
+
+   $dbopts = parse_url($dbUrl);
+
+   $dbHost = $dbopts["host"];
+   $dbPort = $dbopts["port"];
+   $dbUser = $dbopts["user"];
+   $dbPassword = $dbopts["pass"];
+   $dbName = ltrim($dbopts["path"],'/');
+
+   try {
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+   }
+   catch (PDOException $ex) {
+      print "<p>error: $ex->getMessage() </p>\n\n";
+      die();
+   }
+   
+  /* try
    {
       $user = 'postgres';
       $password = '1';
@@ -9,5 +33,5 @@
    {
       echo 'Error!: ' . $ex->getMessage();
       die();
-   }
+   }*/
 ?>
